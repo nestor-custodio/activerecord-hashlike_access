4b7dfda856a744f37c86739d00159d28c5584580
Nestor Custodio <nestor@custodio.org>
2026-08-06 01:39:29 -0500

    init: `bundle gem activerecord-hashlike_access`

---
+++ lib/activerecord/hashlike_access.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "hashlike_access/version"
+
+module Activerecord
+  module HashlikeAccess
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/activerecord/hashlike_access/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Activerecord
+  module HashlikeAccess
+    VERSION = "0.1.0"
+  end
+end
